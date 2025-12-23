ab1dda930de3bc6f08163011f09d7921ecc85f71
rohit-bxi <rohit.chaudhary@bxitech.com>
2025-12-23 17:29:51 +0530

    comments removed

--- code_backend_theme/__manifest__.py
+++ code_backend_theme/__manifest__.py
@@ -1,34 +1,14 @@
 # -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Swaraj R (odoo@cybrosys.com)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
 {
     "name": "Code Backend Theme",
     "version": "19.0.1.0.0",
     "category": "Themes/Backend",
     "summary": "Code Backend Theme is an attractive theme for backend",
     "description": """Minimalist and elegant theme for Odoo backend""",
-    "author": "Cybrosys Techno Solutions",
-    "company": "Cybrosys Techno Solutions",
-    "maintainer": "Cybrosys Techno Solutions",
-    "website": "https://www.cybrosys.com",
+    "author": "BXI Tech",
+    "company": "BXI Tech",
+    "maintainer": "BXI Tech",
+    "website": "",
     "depends": ["web", "mail"],
     "data": [
         "views/layout_templates.xml",
