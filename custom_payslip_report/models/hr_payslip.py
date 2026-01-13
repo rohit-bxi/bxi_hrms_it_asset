dc64164bc6c650ed6080a4e9a919f65ade550bb2
rohit-bxi <rohit.chaudhary@bxitech.com>
2026-01-13 14:58:40 +0530

    custom payslip in bxi format

---
+++ custom_payslip_report/models/hr_payslip.py
@@ -0,0 +1,15 @@
+from odoo import models
+
+class HrPayslip(models.Model):
+    _inherit = 'hr.payslip'
+
+    def action_print_custom_payslip(self):
+        self.ensure_one()
+        xmlid = "custom_payslip_report.action_custom_payslip_pdf"
+        try:
+            report = self.env.ref(xmlid)
+        except ValueError:
+            raise UserError(_(
+                "Report Not Fount"))
+        return report.report_action(self)
+
