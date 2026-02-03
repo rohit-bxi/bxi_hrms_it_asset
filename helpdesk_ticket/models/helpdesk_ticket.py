812dca145ed6d664dfbb72c0abc3e757094a5be1
rohit-bxi <rohit.chaudhary@bxitech.com>
2026-02-03 18:15:03 +0530

    python domain removed

--- helpdesk_ticket/models/helpdesk_ticket.py
+++ helpdesk_ticket/models/helpdesk_ticket.py
@@ -26,24 +26,24 @@ class HelpdeskTicket(models.Model):
     )
 
 
-    @api.onchange('team_id')
-    def _onchange_team_id_category(self):
-        self.category_id = False
-        self.sub_category_id = False
-        return {
-            'domain': {
-                'category_id': [('team_id', '=', self.team_id.id)]
-            }
-        }
+    # @api.onchange('team_id')
+    # def _onchange_team_id_category(self):
+    #     self.category_id = False
+    #     self.sub_category_id = False
+    #     return {
+    #         'domain': {
+    #             'category_id': [('team_id', '=', self.team_id.id)]
+    #         }
+    #     }
 
-    @api.onchange('category_id')
-    def _onchange_category_id_subcategory(self):
-        self.sub_category_id = False
-        return {
-            'domain': {
-                'sub_category_id': [('category_id', '=', self.category_id.id)]
-            }
-        }
+    # @api.onchange('category_id')
+    # def _onchange_category_id_subcategory(self):
+    #     self.sub_category_id = False
+    #     return {
+    #         'domain': {
+    #             'sub_category_id': [('category_id', '=', self.category_id.id)]
+    #         }
+    #     }
 
     # @api.model
     # def message_new(self, msg_dict, custom_values=None):
