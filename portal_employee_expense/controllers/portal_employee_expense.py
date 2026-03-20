d29ef2a0e6b5bfc40e5e6e1f4d2764c26c9875c5
Ritika Shekhawat <ritika.shekhawat@bxitech.com>
2026-03-20 16:13:35 +0530

    portal_employee_expense grant access admin and portal

--- portal_employee_expense/controllers/portal_employee_expense.py
+++ portal_employee_expense/controllers/portal_employee_expense.py
@@ -9,19 +9,25 @@ class EmployeePortalExpense(http.Controller):
     @http.route(['/my/employee-expenses'], type='http', auth='user', website=True)
     def portal_employee_expenses(self, **kwargs):
         user = request.env.user
-        employee = request.env['hr.employee'].sudo().search([
-            ('user_id', '=', user.id)
-        ], limit=1)
-        expenses = []
-        if employee:
-            expenses = request.env['hr.expense'].sudo().search([
-                ('employee_id', '=', employee.id)
-            ])
+        Expense = request.env['hr.expense'].sudo()
+
+        if user.has_group('base.group_user'):
+            expenses = Expense.search([])
+        else:
+            employee = request.env['hr.employee'].sudo().search([
+                ('user_id', '=', user.id)
+            ], limit=1)
+
+            expenses = []
+            if employee:
+                expenses = Expense.search([
+                    ('employee_id', '=', employee.id)
+                ])
         values = {
             'expenses': expenses,
         }
         return request.render('portal_employee_expense.portal_my_expenses_template', values)
-
+    
     @http.route('/my/submit-expenses', type='http', auth="user", website=True)
     def submit_expenses(self, **post):
 
