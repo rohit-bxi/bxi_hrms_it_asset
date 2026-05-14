30b83ae1a53883ab9778fbd89b1a1216f92d2578
rohit-bxi <rohit.chaudhary@bxitech.com>
2026-05-14 11:14:49 +0530

    Payment API ICICI

---
+++ ICIC_bank/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ICICI Bank',
+    'category': 'Human Resources',
+    'version': '19.0.1.0.0',
+    'sequence': 1,
+    'author': 'BXI',
+    'depends': ['base'],
+    'summary': 'Bank Integration ICICI',
+    'description': 'Bank Integration ICICI',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
